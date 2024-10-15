4fe27070bfb9aace980b3ea785b8411c84575535
uriGrif <urielgrif@gmail.com>
2024-10-15 15:54:08 -0300

    mas tablas (faltan solo las relacionadas a producto)

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -64,3 +64,68 @@ CREATE TABLE PIZZA_VIERNES_UADE.factura (
 	total decimal(18,0),
 	cod_vendedor decimal(18,0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.vendedor(cod_vendedor)
 );
+
+CREATE TABLE PIZZA_VIERNES_UADE.concepto_factura (
+	cod_concepto_factura decimal(18,0) PRIMARY KEY,
+	concepto nvarchar(50)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.producto_modelo (
+	cod_modelo decimal(18,0) PRIMARY KEY,
+	descripcion nvarchar(50)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.producto_marca (
+	cod_marca decimal(18,0) PRIMARY KEY,
+	descripcion nvarchar(50)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.rubro (
+	cod_rubro decimal(18,0) PRIMARY KEY,
+	descripcion nvarchar(50)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.tipo_medio_pago (
+	cod_tipo_medio_pago decimal(18,0) PRIMARY KEY,
+	descripcion nvarchar(50)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.medio_pago (
+	cod_medio_pago decimal(18,0) PRIMARY KEY,
+	medio nvarchar(50),
+	cod_tipo_medio_pago decimal(18,0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.tipo_medio_pago(cod_tipo_medio_pago)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.venta (
+	cod_venta decimal(18,0) PRIMARY KEY,
+	fecha_hora datetime,
+	total decimal(18,2),
+	cod_cliente decimal(18,0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.cliente(cod_cliente)
+);
+
+
+CREATE TABLE PIZZA_VIERNES_UADE.pago (
+	nro_pago decimal(18,0) PRIMARY KEY,
+	importe decimal(18,2),
+	cod_venta decimal(18,0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.venta(cod_venta),
+	cod_medio_pago decimal(18,0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.medio_pago(cod_medio_pago)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.detalle_pago (
+	nro_pago decimal(18,0) PRIMARY KEY FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.pago(nro_pago),
+	nro_tarjeta nvarchar(50),
+	fecha_ven_tarjeta date,
+	cant_cuotas decimal(18,0)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.envio (
+	nro_envio decimal(18,0) PRIMARY KEY,
+	fecha_programada date,
+	hora_inicio decimal(18,0),
+	hora_fin decimal(18,0),
+	costo decimal(18,2),
+	fecha_hora_entrega datetime,
+	tipo nvarchar(50),
+	cod_venta decimal(18,0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.venta(cod_venta),
+	cod_domicilio decimal(18,0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.domicilio(cod_domicilio)
+);
