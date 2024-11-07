f748e3fdf8b336f3c007a6b3444b7d1b02e3430b
Gonzalo Ivo Turri <gturri@frba.utn.edu.ar>
2024-11-07 12:00:10 -0300

    BIlateral Oneroso y conmutativo
    Co-Authored-By: Matías Jastrebow <mjastrebow@frba.utn.edu.ar>
    Co-Authored-By: Santiago Mendez <samendez@frba.utn.edu.ar>
    Co-Authored-By: Uriel Grifman <ugrifman@frba.utn.edu.ar>

--- data/script_creacion_BI.sql
+++ data/script_creacion_BI.sql
@@ -11,16 +11,19 @@ CREATE TABLE PIZZA_VIERNES_UADE.BI_ubicacion (
     localidad NVARCHAR(50)
 );
 
-CREATE TABLE PIZZA_VIERNES_UADE.BI_producto (
+CREATE TABLE PIZZA_VIERNES_UADE.BI_marca (
     id DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
-    marca NVARCHAR(50),
-    modelo NVARCHAR(50)
+    marca NVARCHAR(50)
 );
 
 CREATE TABLE PIZZA_VIERNES_UADE.BI_rubro (
     id DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
     rubro NVARCHAR(50),
     subrubro NVARCHAR(50)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.BI_publicacion (
+    cod_publicacion DECIMAL(18,0) PRIMARY KEY
 );
 
 CREATE TABLE PIZZA_VIERNES_UADE.BI_tipo_medio_pago (
@@ -43,24 +46,80 @@ CREATE TABLE PIZZA_VIERNES_UADE.BI_tipo_envio (
     tipo NVARCHAR(50)
 );
 
+CREATE TABLE PIZZA_VIERNES_UADE.BI_envio (
+    id DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1)
+);
+
 CREATE TABLE PIZZA_VIERNES_UADE.BI_concepto_facturacion (
     id DECIMAL(18,0) PRIMARY KEY IDENTITY(1,1),
     tipo NVARCHAR(50)
 );
 
-CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos (
+-- CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos (
+--     tiempo_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tiempo(id) NOT NULL,
+--     ubicacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
+--     rubro_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rubro(id) NOT NULL,
+--     tipo_medio_pago_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tipo_medio_pago(id) NOT NULL,
+--     rango_etario_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rango_etario_clientes(id) NOT NULL,
+--     tipo_envio_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tipo_envio(id) NOT NULL,
+--     rango_horario_ventas_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rango_horario_ventas(id) NOT NULL,
+--     concepto_facturacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_concepto_facturacion(id) NOT NULL,
+--     producto_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_producto(id) NOT NULL,
+--     total_facturado DECIMAL(18,2),
+--     costo_envio DECIMAL(18,2),
+--     total_pagado DECIMAL(18,2),
+--     PRIMARY KEY(tiempo_id, ubicacion_id, rubro_id, tipo_medio_pago_id, rango_etario_id,
+--      tipo_envio_id, rango_horario_ventas_id, concepto_facturacion_id, producto_id)
+-- );
+
+CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_facturacion (
+    tiempo_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tiempo(id) NOT NULL,
+    ubicacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
+    concepto_facturacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_concepto_facturacion(id) NOT NULL,
+    total_facturado DECIMAL(18,2)
+    PRIMARY KEY(tiempo_id, ubicacion_id, concepto_facturacion_id)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_envios (
+    tiempo_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tiempo(id) NOT NULL,
+    ubicacion_cliente_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
+    tipo_envio_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tipo_envio(id) NOT NULL,
+    envio_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_envio(id) NOT NULL,
+    estado CHAR(1), -- 'C' para cumplido 'N' para NO cumplido
+    costo_envio DECIMAL(18,2)
+    PRIMARY KEY(tiempo_id, ubicacion_cliente_id, tipo_envio_id,envio_id)
+);
+
+
+CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_pagos (
+    tiempo_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tiempo(id) NOT NULL,
+    ubicacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
+    tipo_medio_pago_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tipo_medio_pago(id) NOT NULL,
+    total_pagos_en_cuotas DECIMAL(18,2),
+    total_pagos_sin_cuotas DECIMAL(18,2)
+    PRIMARY KEY(tiempo_id, ubicacion_id, tipo_medio_pago_id)
+);
+
+
+CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_ventas (
+    tiempo_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tiempo(id) NOT NULL,
+    ubicacion_almacenes_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
+    rubro_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rubro(id) NOT NULL,
+    rango_etario_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rango_etario_clientes(id) NOT NULL,
+    ubicacion_clientes_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
+    rango_horario_ventas_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rango_horario_ventas(id) NOT NULL,
+    valor_ventas DECIMAL(18,2),
+    cantidad_ventas DECIMAL(18,2)
+    PRIMARY KEY(tiempo_id, ubicacion_almacenes_id, rubro_id,rango_etario_id,ubicacion_clientes_id,rango_horario_ventas_id)
+);
+
+CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_publicaciones (
     tiempo_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tiempo(id) NOT NULL,
     ubicacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
     rubro_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rubro(id) NOT NULL,
-    tipo_medio_pago_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tipo_medio_pago(id) NOT NULL,
-    rango_etario_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rango_etario_clientes(id) NOT NULL,
-    tipo_envio_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tipo_envio(id) NOT NULL,
-    rango_horario_ventas_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rango_horario_ventas(id) NOT NULL,
-    concepto_facturacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_concepto_facturacion(id) NOT NULL,
-    producto_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_producto(id) NOT NULL,
-    total_facturado DECIMAL(18,2),
-    costo_envio DECIMAL(18,2),
-    total_pagado DECIMAL(18,2),
-    PRIMARY KEY(tiempo_id, ubicacion_id, rubro_id, tipo_medio_pago_id, rango_etario_id,
-     tipo_envio_id, rango_horario_ventas_id, concepto_facturacion_id, producto_id)
+    marca_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_marca(id) NOT NULL,
+    publicacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_publicacion(cod_publicacion) NOT NULL,
+    dias_vigencia_publicacion DECIMAL(18,0),
+    stock_inicial DECIMAL(18,0)
+    PRIMARY KEY(tiempo_id, ubicacion_id, rubro_id,marca_id,publicacion_id)
 );
