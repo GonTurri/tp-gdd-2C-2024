cfa130130b6e42a23345a3bf23fd91bd6a541e9b
santymendez <santiagomendez532@gmail.com>
2024-11-10 12:15:40 -0300

    facts parte 2
    Co-authored-by: Matías Jastrebow <mjastrebow@frba.utn.edu.ar>
    Co-authored-by: Gonzalo Turri <gturri@frba.utn.edu.ar>
    Co-authored-by: Uriel Grifman <ugrifman@frba.utn.edu.ar>

--- data/script_creacion_BI.sql
+++ data/script_creacion_BI.sql
@@ -73,7 +73,6 @@ CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_envios (
     PRIMARY KEY(tiempo_id, ubicacion_cliente_id, tipo_envio_id,envio_id)
 );
 
-
 CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_pagos (
     tiempo_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tiempo(id) NOT NULL,
     ubicacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
@@ -83,7 +82,6 @@ CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_pagos (
     PRIMARY KEY(tiempo_id, ubicacion_id, tipo_medio_pago_id)
 );
 
-
 CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_ventas (
     tiempo_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tiempo(id) NOT NULL,
     ubicacion_almacenes_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
@@ -92,19 +90,18 @@ CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_ventas (
     ubicacion_clientes_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
     rango_horario_ventas_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rango_horario_ventas(id) NOT NULL,
     valor_ventas DECIMAL(18,2),
-    cantidad_ventas DECIMAL(18,2)
+    cantidad_ventas DECIMAL(18,0)
     PRIMARY KEY(tiempo_id, ubicacion_almacenes_id, rubro_id,rango_etario_id,ubicacion_clientes_id,rango_horario_ventas_id)
 );
 
 CREATE TABLE PIZZA_VIERNES_UADE.BI_hechos_publicaciones (
     tiempo_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_tiempo(id) NOT NULL,
-    ubicacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_ubicacion(id) NOT NULL,
     rubro_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_rubro(id) NOT NULL,
     marca_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_marca(cod_marca) NOT NULL,
     publicacion_id DECIMAL(18, 0) FOREIGN KEY REFERENCES PIZZA_VIERNES_UADE.BI_publicacion(cod_publicacion) NOT NULL,
     dias_vigencia_publicacion DECIMAL(18,0),
     stock_inicial DECIMAL(18,0)
-    PRIMARY KEY(tiempo_id, ubicacion_id, rubro_id,marca_id,publicacion_id)
+    PRIMARY KEY(tiempo_id, rubro_id,marca_id,publicacion_id)
 );
 
 -- CREACION DE VISTAS ?? 
@@ -132,7 +129,7 @@ END
 
 GO
 
-CREATE FUNCTION PIZZA_VIERNES_UADE.get_rango_horario (@horario DATE) RETURNS INT AS
+CREATE FUNCTION PIZZA_VIERNES_UADE.get_rango_horario (@horario DATETIME) RETURNS INT AS
 BEGIN
     DECLARE @hora INT;
 
@@ -142,7 +139,7 @@ BEGIN
     SET @id_rango = CASE WHEN @hora >= 0 AND @hora < 6 THEN 1
                          WHEN @hora >= 6 AND @hora < 12 THEN 2
                          WHEN @hora >= 12 AND @hora < 18 THEN 3
-                         WHEN @hora BETWEEN 18 AND 24 THEN 4
+                         WHEN @hora >= 18 AND @hora < 24 THEN 4
                     END;
     RETURN @id_rango
 END
@@ -164,15 +161,39 @@ BEGIN
     RETURN @cuatrimestre;
 END
 
+GO
+
 CREATE FUNCTION PIZZA_VIERNES_UADE.get_tiempo (@fecha DATE) RETURNS INT AS
 BEGIN
     DECLARE @tiempo_id INTEGER;
 
-    SET @tiempo_id = SELECT id FROM PIZZA_VIERNES_UADE.BI_tiempo
-                        WHERE anio = YEAR(@fecha) AND mes = MONTH(@fecha);
+    SET @tiempo_id = (SELECT id FROM PIZZA_VIERNES_UADE.BI_tiempo
+                        WHERE anio = YEAR(@fecha) AND mes = MONTH(@fecha));
                         
     RETURN @tiempo_id;
 END
+
+GO
+
+CREATE FUNCTION PIZZA_VIERNES_UADE.get_estado_envio(@fecha_programada DATE,
+                                                    @hora_inicio DECIMAL(18,0),
+                                                    @hora_fin DECIMAL(18,0),
+                                                    @fecha_hora_entrega DATETIME) RETURNS CHAR(1) AS
+BEGIN
+    DECLARE @estado CHAR(1);
+    DECLARE @fecha_entrega DATE;
+    DECLARE @hora_entrega DECIMAL(18,0);
+
+    SET @fecha_entrega = CONVERT(DATE, @fecha_hora_entrega);
+    SET @hora_entrega = DATEPART(HOUR, @fecha_hora_entrega);
+
+    SET @estado = CASE WHEN @fecha_hora_entrega IS NOT NULL AND @fecha_entrega <= @fecha_programada 
+                    AND @hora_entrega BETWEEN @hora_inicio AND @hora_fin
+                    THEN 'C' ELSE 'N'
+    END;
+
+    RETURN @estado;
+END;
 
 -- MIGRACION DE TABLAS
 
@@ -220,7 +241,7 @@ BEGIN
     VALUES ('0:00 - 6:00'), ('6:00 - 12:00'), ('12:00 - 18:00'), ('18:00 - 24:00');
     
     -- LLENADO DE DIMENSION UBICACION
-    INSERT INTO PIZZA_VIERNES_UADE.BI_ubicacion (provincia, localidad)
+    INSERT INTO PIZZA_VIERNES_UADE.BI_ubicacion (localidad, provincia)
     SELECT DISTINCT l.nom_localidad, p.nom_provincia
     FROM PIZZA_VIERNES_UADE.localidad l
     JOIN PIZZA_VIERNES_UADE.provincia p ON p.cod_provincia = l.cod_provincia
@@ -247,4 +268,91 @@ END
 
 GO 
 
+CREATE OR ALTER PROCEDURE PIZZA_VIERNES_UADE.BI_llenar_hechos AS
+BEGIN
+
+    -- LLENADO DE HECHOS DE FACTURACION
+    INSERT INTO PIZZA_VIERNES_UADE.BI_hechos_facturacion (tiempo_id, ubicacion_id, concepto_facturacion_id, total_facturado)
+    SELECT PIZZA_VIERNES_UADE.get_tiempo(f.fecha), u.id, bcp.id, SUM(df.sub_total)
+    FROM PIZZA_VIERNES_UADE.factura f
+    INNER JOIN PIZZA_VIERNES_UADE.detalle_factura df ON df.nro_factura = f.nro_factura
+    INNER JOIN PIZZA_VIERNES_UADE.concepto_factura cf ON cf.cod_concepto_factura = df.cod_concepto_factura
+    INNER JOIN PIZZA_VIERNES_UADE.vendedor v ON v.cod_vendedor = f.cod_vendedor
+    INNER JOIN PIZZA_VIERNES_UADE.domicilio d ON d.cod_usuario = v.cod_usuario
+    INNER JOIN PIZZA_VIERNES_UADE.localidad l ON l.cod_localidad = d.cod_localidad  
+    INNER JOIN PIZZA_VIERNES_UADE.provincia p ON p.cod_provincia = l.cod_provincia
+    INNER JOIN PIZZA_VIERNES_UADE.BI_concepto_facturacion bcp ON bcp.tipo = cf.concepto
+    INNER JOIN PIZZA_VIERNES_UADE.BI_ubicacion u ON u.provincia = p.nom_provincia AND u.localidad = l.nom_localidad
+    GROUP BY PIZZA_VIERNES_UADE.get_tiempo(f.fecha), u.id, bcp.id
+
+    -- LLENADO DE HECHOS DE PAGO
+    INSERT INTO PIZZA_VIERNES_UADE.BI_hechos_pagos (tiempo_id, ubicacion_id, tipo_medio_pago_id, total_pagos_en_cuotas, total_pagos_sin_cuotas)
+    SELECT PIZZA_VIERNES_UADE.get_tiempo(p.fecha), u.id, btmp.id,  
+    COALESCE(SUM(CASE WHEN dp.cant_cuotas > 1 THEN p.importe ELSE 0 END), 0) AS total_pagos_en_cuotas,
+    COALESCE(SUM(CASE WHEN dp.cant_cuotas = 1 THEN p.importe ELSE 0 END), 0) AS total_pagos_sin_cuotas
+    FROM PIZZA_VIERNES_UADE.pago p
+    INNER JOIN PIZZA_VIERNES_UADE.medio_pago mp ON mp.cod_medio_pago = p.cod_medio_pago
+    INNER JOIN PIZZA_VIERNES_UADE.detalle_pago dp ON dp.nro_pago = p.nro_pago
+    INNER JOIN PIZZA_VIERNES_UADE.tipo_medio_pago tmp ON tmp.cod_tipo_medio_pago = mp.cod_tipo_medio_pago
+    INNER JOIN PIZZA_VIERNES_UADE.envio e ON e.cod_venta = p.cod_venta
+    INNER JOIN PIZZA_VIERNES_UADE.domicilio d ON d.cod_domicilio = e.cod_domicilio
+    INNER JOIN PIZZA_VIERNES_UADE.localidad l ON l.cod_localidad = d.cod_localidad  
+    INNER JOIN PIZZA_VIERNES_UADE.provincia pr ON pr.cod_provincia = l.cod_provincia
+    INNER JOIN PIZZA_VIERNES_UADE.BI_ubicacion u ON u.provincia = pr.nom_provincia AND u.localidad = l.nom_localidad
+    INNER JOIN PIZZA_VIERNES_UADE.BI_tipo_medio_pago btmp ON btmp.descripcion = tmp.descripcion
+    GROUP BY PIZZA_VIERNES_UADE.get_tiempo(p.fecha), u.id, btmp.id;
+
+    -- LLENADO DE HECHOS PUBICACIONES
+    INSERT INTO PIZZA_VIERNES_UADE.BI_hechos_publicaciones (tiempo_id, rubro_id, marca_id, publicacion_id, dias_vigencia_publicacion, stock_inicial)
+    SELECT PIZZA_VIERNES_UADE.get_tiempo(p.fecha_inicio), br.id, bm.cod_marca, bp.cod_publicacion, DATEDIFF(DAY, p.fecha_inicio, p.fecha_fin), p.stock
+    FROM PIZZA_VIERNES_UADE.publicacion p
+    INNER JOIN PIZZA_VIERNES_UADE.producto pr ON pr.id_producto = p.id_producto
+    INNER JOIN PIZZA_VIERNES_UADE.producto_marca pm ON pm.cod_marca = pr.cod_marca
+    INNER JOIN PIZZA_VIERNES_UADE.subrubro s ON s.cod_subrubro = pr.cod_subrubro
+    INNER JOIN PIZZA_VIERNES_UADE.rubro r ON r.cod_rubro = s.cod_rubro
+    INNER JOIN PIZZA_VIERNES_UADE.BI_rubro br ON br.rubro = r.descripcion AND br.subrubro = s.descripcion
+    INNER JOIN PIZZA_VIERNES_UADE.BI_marca bm ON bm.marca = pm.descripcion
+    INNER JOIN PIZZA_VIERNES_UADE.BI_publicacion bp ON bp.cod_publicacion = p.cod_publicacion
+    
+    -- LLENADO DE HECHOS DE VENTAS
+    INSERT INTO PIZZA_VIERNES_UADE.BI_hechos_ventas (tiempo_id, ubicacion_almacenes_id, rubro_id, rango_etario_id, ubicacion_clientes_id, 
+    rango_horario_ventas_id, valor_ventas, cantidad_ventas)
+    SELECT PIZZA_VIERNES_UADE.get_tiempo(v.fecha_hora), ual.id, br.id, PIZZA_VIERNES_UADE.get_rango_edad(c.fecha_nac), ucli.id, 
+        PIZZA_VIERNES_UADE.get_rango_horario(v.fecha_hora), SUM(v.total), COUNT(v.cod_venta)
+    FROM PIZZA_VIERNES_UADE.venta v
+    INNER JOIN PIZZA_VIERNES_UADE.detalle_venta dv ON dv.cod_venta = v.cod_venta
+    INNER JOIN PIZZA_VIERNES_UADE.publicacion p ON p.cod_publicacion = dv.cod_publicacion
+    INNER JOIN PIZZA_VIERNES_UADE.producto pr ON pr.id_producto = p.id_producto
+    INNER JOIN PIZZA_VIERNES_UADE.subrubro s ON s.cod_subrubro = pr.cod_subrubro
+    INNER JOIN PIZZA_VIERNES_UADE.rubro r ON r.cod_rubro = s.cod_rubro
+    INNER JOIN PIZZA_VIERNES_UADE.BI_rubro br ON br.subrubro = s.descripcion AND br.rubro = r.descripcion
+    INNER JOIN PIZZA_VIERNES_UADE.almacen a ON a.cod_almacen = p.cod_almacen
+    INNER JOIN PIZZA_VIERNES_UADE.localidad la ON la.cod_localidad = a.cod_localidad --localidad almacen
+    INNER JOIN PIZZA_VIERNES_UADE.provincia pa ON pa.cod_provincia = la.cod_provincia --provincia almacen
+    INNER JOIN PIZZA_VIERNES_UADE.BI_ubicacion ual ON ual.provincia = pa.nom_provincia AND ual.localidad = la.nom_localidad --ubicacion almacen
+    INNER JOIN PIZZA_VIERNES_UADE.cliente c ON c.cod_cliente = v.cod_cliente
+    INNER JOIN PIZZA_VIERNES_UADE.domicilio d ON d.cod_usuario = c.cod_usuario
+    INNER JOIN PIZZA_VIERNES_UADE.localidad lc ON lc.cod_localidad = d.cod_localidad  --localidad cliente
+    INNER JOIN PIZZA_VIERNES_UADE.provincia pc ON pc.cod_provincia = lc.cod_provincia --provincia cliente
+    INNER JOIN PIZZA_VIERNES_UADE.BI_ubicacion ucli ON ucli.provincia = pc.nom_provincia AND ucli.localidad = lc.nom_localidad --ubicacion cliente
+    GROUP BY PIZZA_VIERNES_UADE.get_tiempo(v.fecha_hora), ual.id, br.id, PIZZA_VIERNES_UADE.get_rango_edad(c.fecha_nac), ucli.id, PIZZA_VIERNES_UADE.get_rango_horario(v.fecha_hora)
+
+    -- LLENADO DE HECHOS DE ENVIO ('C' -> Cumplido - 'N' -> No cumplido)
+    INSERT INTO PIZZA_VIERNES_UADE.BI_hechos_envios (tiempo_id, ubicacion_cliente_id, tipo_envio_id, envio_id, estado, costo_envio)
+    SELECT PIZZA_VIERNES_UADE.get_tiempo(e.fecha_hora_entrega), u.id, bte.id, be.id,
+            PIZZA_VIERNES_UADE.get_estado_envio(e.fecha_programada, e.hora_inicio, e.hora_fin, e.fecha_hora_entrega), e.costo
+    FROM PIZZA_VIERNES_UADE.envio e
+    INNER JOIN PIZZA_VIERNES_UADE.BI_envio be ON be.id = e.nro_envio
+    INNER JOIN PIZZA_VIERNES_UADE.BI_tipo_envio bte ON bte.tipo = e.tipo
+    INNER JOIN PIZZA_VIERNES_UADE.domicilio d ON d.cod_domicilio = e.cod_domicilio
+    INNER JOIN PIZZA_VIERNES_UADE.localidad l ON l.cod_localidad = d.cod_localidad  
+    INNER JOIN PIZZA_VIERNES_UADE.provincia p ON p.cod_provincia = l.cod_provincia 
+    INNER JOIN PIZZA_VIERNES_UADE.BI_ubicacion u ON u.provincia = p.nom_provincia AND u.localidad = l.nom_localidad
+END
+
+GO
+
 EXEC PIZZA_VIERNES_UADE.BI_llenar_dimensiones;
+EXEC PIZZA_VIERNES_UADE.BI_llenar_hechos;
+
+GO
